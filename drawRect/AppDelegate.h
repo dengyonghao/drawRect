435a800d40481543c2726cab92511a52df963df8
dengyonghao <dengyonghao@carlife.local>
2015-08-26 11:49:26 +0800

    Initial Commit

---
+++ drawRect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  drawRect
+//
+//  Created by dengyonghao on 15/8/26.
+//  Copyright (c) 2015年 dengyonghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
